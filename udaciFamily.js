4a03045fbf5612bf67ce942ba25977680c68aa43
siboniseni <siboniseni.kasa@outlook.com>
2023-07-09 10:08:44 +0200

    Arr Quiz: UdaciFamily (6-1)

---
+++ udaciFamily.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `udaciFamily`
+ * - The variable `udaciFamily` should be an array containing the values `"Julia"`, `"James"`, and one other name
+ * - Your code should print `udaciFamily` to the console
+ * - BE CAREFUL ABOUT THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+var udaciFamily = ["Julia", "James", "Siboniseni"];
+
+    for (var index = 0; index < udaciFamily.length; index++) {
+        
+        console.log(udaciFamily[index]);
+    }

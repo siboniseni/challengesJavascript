80608fe0fc4267425992bae1d07be3b82c502f9d
siboniseni <siboniseni.kasa@outlook.com>
2023-07-10 16:16:53 +0200

    objects Quiz: Umbrella (7-1)

---
+++ umbrella.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Umbrella (7-1)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `umbrella`
+ * - The variable `umbrella` should be an object
+ * - Your `umbrella` object should have the `color` and `isOpen` property
+ * - Your `umbrella` object should have an `open()` method that toggles the value of `isOpen` property
+ * - Your `umbrella` object should have an `close()` method that toggles the value of `isOpen`
+ */
+
+var umbrella = {
+    color: "pink",
+    isOpen: true,
+    open: function() {
+        if (umbrella.isOpen === true) {
+            return "The umbrella is already opened!";
+        } else {
+            umbrella.isOpen = true;
+            return "Julia opens the umbrella!";
+        }
+    },
+    closed: function() {
+        if (umbrella.isOpen === false) {
+            return "The umbrella is already closed!";
+        } else {
+            umbrella.isOpen = false;
+            return "Julia closes the umbrella!";
+        }
+    },
+};
+console.log(umbrella.open())
